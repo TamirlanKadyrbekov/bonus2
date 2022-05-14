1e6cbc31e5688cc95a6e9dbe2abbd134d3ec2ada
TamirlanKadyrbekov <79455936+TamirlanKadyrbekov@users.noreply.github.com>
2022-05-14 17:15:56 +0600

    Add files via upload

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+
+	file, err := os.OpenFile(os.Args[-1], flags, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	zipWriter := zip.NewWriter(file)
+	defer zipWriter.Close()
+
+	for i := 1; i < len(os.Args)-2; i++ {
+		filename := os.Args[i]
+		print(i)
+		if err := AddFileToZip(zipWriter, filename); err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+func AddFileToZip(zipWriter *zip.Writer, filename string) error {
+	fileToZip, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer fileToZip.Close()
+
+	info, err := fileToZip.Stat()
+	if err != nil {
+		return err
+	}
+
+	splitted_filename := strings.Split(filename, "/")
+	filename_new := splitted_filename[len(splitted_filename)-1]
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+
+	header.Name = filename_new
+
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(writer, fileToZip)
+	return err
+}
